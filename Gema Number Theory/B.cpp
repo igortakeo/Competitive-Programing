5c224bcdb3d2ea4b8066291a2c27789254f2058b
igortakeo <igor_takeo77@hotmail.com>
2019-06-16 01:54:50 -0300

    Update from my computer

---
+++ Gema Number Theory/B.cpp
@@ -0,0 +1,11 @@
+//
+// Created by igortakeo on 16/06/19.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin >> n;
+    cout << n/2520 << endl;
+    return 0;
+}
