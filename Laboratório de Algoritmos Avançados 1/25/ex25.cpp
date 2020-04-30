08b66090ba7fd7b01808b7c1dde6fb300f1a2e71
igortakeo <igor_takeo77@hotmail.com>
2020-04-30 01:54:34 -0300

    updated ex25.cpp

--- Laboratório de Algoritmos Avançados 1/25/ex25.cpp
+++ Laboratório de Algoritmos Avançados 1/25/ex25.cpp
@@ -10,7 +10,7 @@ void solve(vector<int>&v, int l, int r, int k){
 	bool flag = true;
 	int cnt = 0;
 	int pos = 0;
-	
+	int b = 0;
 	int mid  = (l+r)/2;
 
 	while(v[pos] < (int)v.back()){
@@ -20,16 +20,19 @@ void solve(vector<int>&v, int l, int r, int k){
 		int it = lower_bound(v.begin(), v.end(), x) - v.begin();
 		if(v[it] > x or it == (int)v.size())it--;
 		
+		int diff = v[it]-v[pos];
+
 		if(it == pos){
 			flag = false;
 			break;
 		}
 		else cnt++;
+		b = max(b, diff);
 		
 		pos = it;
 	}	
 	
-	if(flag and cnt-1 <= k) pans.push_back(mid);
+	if(flag and cnt-1 <= k) pans.push_back(b);
 	
 	if(l > r or mid == l or mid == r) return;
 	
@@ -51,10 +54,12 @@ int main(){
 			cin >> x;
 			v[i] = v[i-1]+x;	 
 		}
-
-		solve(v, 0, v[n+1], k);
-		int ans = *min_element(pans.begin(), pans.end());		
-		
+		int ans = 0;
+		if(k == 0) ans = v[n+1]; 
+		else{
+			solve(v, 0, v[n+1], k);
+			ans = *min_element(pans.begin(), pans.end());		
+		}
 		cout << ans << endl;
 		
 		pans.clear();
